039bfa734c79007c3806aaddf8ce8bfe73f6e8f4
nttu <nttugpt@gmail.com>
2023-07-05 13:59:32 +0700

    Init source with 2 modules: Admin & Library. Add two models: Admin & Role into Library module

---
+++ Library/src/main/java/com/ecommerce/library/model/Admin.java
@@ -0,0 +1,44 @@
+package com.ecommerce.library.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Collection;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "admins")
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @Column(name = "admin_id")
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String username;
+
+    private String password;
+
+    @Lob
+    @Column(columnDefinition = "MEDIUMBLOB")
+    private String image;
+
+    // Cascade All giúp tự động lưu hặc xoá các record của table tham chiếu đến table này.
+    // Ví dụ: khi xoá admin thì xoá luôn record ở table admin_roles mà nó thamc chiếu đến admin này
+    // Fetch Eager: khi truy vấn dữ liệu, nó load het các dữ liệu tham chiếu luôn
+    // Còn lazy chỉ load mỗi bảng đó, khi cần mới load tiếp
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name="admin_roles",
+            joinColumns = @JoinColumn(name="admin_id", referencedColumnName = "admin_id"),
+            inverseJoinColumns = @JoinColumn(name="role_id", referencedColumnName = "role_id")
+    )
+    private Collection<Role> roles;
+}
