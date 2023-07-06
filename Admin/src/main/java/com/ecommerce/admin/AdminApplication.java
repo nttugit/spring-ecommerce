4d16f15840618accd3b0dd0eaaaea2a2adb6b293
nttu <nttugpt@gmail.com>
2023-07-06 10:51:36 +0700

    Add Admin bootstrap template, register Admin

--- Admin/src/main/java/com/ecommerce/admin/AdminApplication.java
+++ Admin/src/main/java/com/ecommerce/admin/AdminApplication.java
@@ -3,12 +3,14 @@ package com.ecommerce.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication(scanBasePackages = {"com.ecommerce.library.*", "com.ecommerce.*"})
-@EnableJpaRepositories(value = "com.ecommerce.library.repository")
+@SpringBootApplication(scanBasePackages = {"com.ecommerce.library.*", "com.ecommerce.admin.*"})@EnableJpaRepositories(value = "com.ecommerce.library.repository")
 @EntityScan(value="com.ecommerce.library.model")
-public class AdminApplication {
+@Configuration
+public class AdminApplication  implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdminApplication.class, args);
--- Admin/src/main/java/com/ecommerce/admin/controller/LoginController.java
+++ Admin/src/main/java/com/ecommerce/admin/controller/LoginController.java
@@ -1,13 +1,79 @@
 package com.ecommerce.admin.controller;
 
+import com.ecommerce.library.dto.AdminDto;
+import com.ecommerce.library.model.Admin;
+import com.ecommerce.library.service.impl.AdminServiceImpl;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
+    @Autowired
+    private AdminServiceImpl adminService;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
     @GetMapping("/login")
     public String loginForm(){
         return "login";
+    }
+
+
+    @GetMapping("/register")
+    public String register(Model model){
+        model.addAttribute("adminDto", new AdminDto());
+        return "register";
+    }
+
+    @GetMapping("/forgot-password")
+    public String forgotPassword(Model model){
+        return "forgot-password";
+    }
+
+    @PostMapping("/register-new")
+    public String addNewAdmin(
+            @ModelAttribute("adminDto")AdminDto adminDto,
+            BindingResult result,
+            Model model,
+            HttpSession session
+    ){
+        try {
+            if(result.hasErrors()){
+                model.addAttribute("adminDto",adminDto);
+                return "register";
+            }
+            String username = adminDto.getUsername();
+            Admin admin = adminService.findByUsername(username);
+
+            model.addAttribute("adminDto",adminDto);
+
+            if(admin != null){
+                session.setAttribute("message","Your email has been registered");
+                return "register";
+            }
+            if(adminDto.getPassword().equals((adminDto.getRepeatPassword()))){
+                adminDto.setPassword(passwordEncoder.encode(adminDto.getPassword()));
+                adminService.save(adminDto);
+                session.setAttribute("message","Register successfully");
+            }else{
+                session.setAttribute("message", "The passwords didn't match");
+                return "register";
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+            session.setAttribute("message", "Error: Cannot register. Please try again.");
+        }
+
+        return "register";
     }
 }
